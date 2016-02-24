ecab69de5cfef0b77abe7ed3a10384f424a0a4f1
Shane Williamson (saw056) <saw056@mail.usask.ca>
2016-02-24 03:38:02 -0600

    Revised the python script
    Now it does not require new authentication every time on the server.
    I've pre set up the repo by cloning with
    "git clone
    https://username:password@github.com/ShaneWilliamson/QM-Lab.git"

--- src/Python Service Script/git-get-service.py
+++ src/Python Service Script/git-get-service.py
@@ -1,16 +1,18 @@
 import time
 import os
+import git
 from subprocess import call
 
 #The routine to run, every 120 seconds it pulls from GitHub
 def start():
   #This should execute the pull into the shared directory folder
-  os.chdir("/home/saw056/QM-Lab")
+  os.chdir("/home/shared/QM-Lab_Repo")
   while True:
     gitPull()
-    time.sleep(121)
+    time.sleep(120)
 
 def gitPull():
-  call(["git", "pull", "https://371GitBot:gitbot371@github.com/ShaneWilliamson/QM-Lab.git"])
+  call(["git", "pull"])
+ 
 
 start()
