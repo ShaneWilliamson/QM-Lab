92b3bffa39ffdecc6a6775a2a0182fdc258294d6
Shane Williamson (saw056) <saw056@mail.usask.ca>
2016-02-24 02:52:17 -0600

    new script to pull to VM, and .travis.yml updated
    Python script to pull changes to the repo every minute was added. This
    is not an elegant solution so I might change it in the future.

---
+++ src/Python Service Script/git-get-service.py
@@ -0,0 +1,16 @@
+import time
+import os
+from subprocess import call
+
+#The routine to run, every 60 seconds it pulls from GitHub
+def start():
+  #This should execute the pull into the shared directory folder
+  os.chdir("/home/saw056/QM-Lab")
+  while True:
+    gitPull()
+    time.sleep(60)
+
+def gitPull():
+  call(["git", "pull", "https://371GitBot:gitbot371@github.com/ShaneWilliamson/QM-Lab.git"])
+
+start()
