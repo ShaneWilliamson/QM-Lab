52fa2d779d8cb723428ce22d15c76573061ff292
Shane Williamson (saw056) <saw056@mail.usask.ca>
2016-02-24 03:05:51 -0600

    Updated frequency of python git-get-service script
    Updated the script to pull every 2 minutes.

--- src/Python Service Script/git-get-service.py
+++ src/Python Service Script/git-get-service.py
@@ -2,13 +2,13 @@ import time
 import os
 from subprocess import call
 
-#The routine to run, every 60 seconds it pulls from GitHub
+#The routine to run, every 120 seconds it pulls from GitHub
 def start():
   #This should execute the pull into the shared directory folder
   os.chdir("/home/saw056/QM-Lab")
   while True:
     gitPull()
-    time.sleep(60)
+    time.sleep(120)
 
 def gitPull():
   call(["git", "pull", "https://371GitBot:gitbot371@github.com/ShaneWilliamson/QM-Lab.git"])
