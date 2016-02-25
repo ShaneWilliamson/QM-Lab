f7bdc6312b01c9cd21af7d8e5b359260c2e6c5d4
Shane Williamson (saw056) <saw056@mail.usask.ca>
2016-02-25 04:56:21 -0600

    Updated deployment script to incorporate cleanup
    As well, renamed the script to 'deployToWeb.py'.
    Usage of the script:
      - Begin screen session (e.g. 'screen -dmS nsid-featureX')
      - enter screen session and run 'sudo python deployToWeb.py featureX'
      - detach and allow the deployment to run continually
      - ... profit?
      - reattach to screen session and use Ctrl-C to interrupt the
            script. The script will catch this KeyboardInterrupt exception
            and clean up nicely.
      - If you kill the session with 'kill ...' you'll need to clean up
            manually. Support for graceful cleanup for this to come later.

--- server-scripts/Python Service Script/deployToWeb.py
+++ server-scripts/Python Service Script/deployToWeb.py
@@ -3,40 +3,58 @@ import os
 import sys
 from subprocess import call
 
+###Global variables###
+# Both are assigned to in start() after we check to make sure of the argv count
+repoPath = ""
+webPath = ""
+
 #The routine to run, every 120 seconds it pulls from GitHub
 #This causes everything in the src folder to be put on display on the webserver
 def start():
+  #Using global variables repoPath, webPath
+  global repoPath
+  global webPath
+  #If we do not have correct input amount
   if len(sys.argv) != 2:
     #Improper input, we need exactly 1 additional argument to the script.
     #  - Additional argument: name of new branch to pull the VM's page
     sys.exit()
 
+  #Otherwise it's business as normal; assign to the global variables
+  repoPath = "/home/shared/QM-Lab_Repos/" + str(sys.argv[1]) + "/"
+  webPath = "/var/www/html/demo/" + str(sys.argv[1]) + "/"
   #This should execute the pull into the shared directory folder
-  repoPath = "/home/shared/QM-Lab_Repos/" + str(sys.argv[1])
-  #Otherwise it's business as normal
   #Set up the branch repository
-  webPath = setupBranch(repoPath, str(sys.argv[1]))
+  setupBranch(repoPath, str(sys.argv[1]))
 
   #Pre Condition: directories set up, content is on webpage
 
   #Loop forever!
   while True:
     gitPull()
-    pushContentToWeb(repoPath + "/QM-Lab/", webPath)
+    pushContentToWeb()
     time.sleep(120)
+
 
 def gitPull():
   call(["git", "pull"])
 
-def pushContentToWeb(curPath, targetPath):
-  #Delete the contents of targetPath
-  call(["rm", "-rf", targetPath])
-  call(["mkdir", "-p", targetPath])
-  #Copy new src/ contents into targetPath
-  call(["cp", "-r", curPath + "src/", targetPath])
+
+def pushContentToWeb():
+  #Global vars
+  global repoPath
+  global webPath
+  #Delete the contents of webPath
+  cleanUpWebPath()
+  call(["mkdir", "-p", webPath])
+  #Copy new src/ contents into webPath
+  call(["cp", "-r", repoPath + "QM-Lab/src/", webPath])
+
 
 # Routine to create a new directory in /var/www/html/ based on input
 def setupBranch(newPath, branchName):
+  #Global vars
+  global webPath
   #First make sure that the top level set of repos exist.
   call(["mkdir", "-p", newPath])
   #If the working directory is not empty... make it empty!
@@ -52,15 +70,43 @@ def setupBranch(newPath, branchName):
   os.chdir(newPath + "/QM-Lab")
   #Checkout the desired branch
   call(["git", "checkout", branchName])
-  #Set up the webpage directory:
-  webPath = "/var/www/html/demo/" + branchName
   call(["mkdir", "-p", webPath])
   #If the working directory is not empty... make it empty!
   if os.listdir(webPath) != []:
     call(["rm", "-rf", webPath])
     #Remake the directory
     call(["mkdir", "-p", webPath])
-  return webPath
 
 
-start()
+def cleanUp():
+  cleanUpRepoPath()
+  cleanUpWebPath()
+
+
+def cleanUpRepoPath():
+  #Global vars
+  global repoPath
+  #Clean up the repository
+  call(["rm", "-rf", repoPath])
+
+
+def cleanUpWebPath():
+  #Global vars
+  global webPath
+  #Clean up the apache directory
+  call(["rm", "-rf", webPath])
+
+
+try:
+  #Run the program until a keyboard interrupt is caught.
+  start()
+except KeyboardInterrupt:
+  #Global vars
+  global repoPath
+  global webPath
+  if repoPath != "" and webPath != "":
+    print("Interrupt received, cleaning up...")
+    cleanUp()
+    print("Clean up complete, exiting program.")
+  else:
+    print("Interrupt received, exiting program.")
