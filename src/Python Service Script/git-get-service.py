f47df04489c06f75f50762e3eba3dbb7de316484
Shane Williamson (saw056) <saw056@mail.usask.ca>
2016-02-24 20:26:20 -0600

    Python server script updated
    Committing changes to python script so that Connor and I can begin
    collaborative work on it.

--- src/Python Service Script/git-get-service.py
+++ src/Python Service Script/git-get-service.py
@@ -1,12 +1,17 @@
 import time
 import os
-import git
+import sys
 from subprocess import call
 
 #The routine to run, every 120 seconds it pulls from GitHub
 def start():
   #This should execute the pull into the shared directory folder
-  os.chdir("/home/shared/QM-Lab_Repo")
+  var repoPath = "/home/shared/QM-Lab_Repo"
+  if len(sys.argv) == 2:
+    displayBranchOnVM(str(sys.argv[1]))
+  #First make sure that the top level set of repos exist.
+  call(["mkdir", "-p", repoPath])
+  os.chdir(repoPath)
   while True:
     gitPull()
     time.sleep(120)
@@ -14,5 +19,10 @@ def start():
 def gitPull():
   call(["git", "pull"])
  
+# TODO: routine to create a new directory in /var/www/html/ based on input
+def displayBranchOnVM(branchName, curPath):
+  #TODO: call([])
+  return curPath + "/" + branchName
+
 
 start()
