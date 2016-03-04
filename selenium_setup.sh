d2f64ee12f5b59127be8ecd600075b32d7461dc5
Shane Williamson <williamson.shane.a@gmail.com>
2016-03-04 17:08:00 -0600

    Sudo was throwing error, fix attempt
    Now travis won't run sudo commands.

--- selenium_setup.sh
+++ selenium_setup.sh
@@ -4,33 +4,33 @@
 # http://programmingarehard.com/2014/03/17/behat-and-selenium-in-vagrant.html
 
 echo "\r\nUpdating system ...\r\n"
-sudo apt-get update
+apt-get update
 
 
 # Create folder to place selenium in
 #
 echo "\r\nCreating folder to place selenium in ...\r\n"
-sudo mkdir ~/selenium
+mkdir ~/selenium
 cd ~/selenium
 
 
 # Get Selenium and install headless Java runtime
 #
 echo "\r\nInstalling Selenium and headless Java runtime ...\r\n"
-sudo wget http://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar
-sudo apt-get -y install openjdk-8-jre-headless
+wget http://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar
+apt-get -y install openjdk-8-jre-headless
 
 
 # Install Firefox
 #
 echo "\r\nInstalling Firefox ...\r\n"
-sudo apt-get -y install firefox
+apt-get -y install firefox
 
 
 # Install headless GUI for firefox.  'Xvfb is a display server that performs graphical operations in memory'
 #
 echo "\r\nInstalling XVFB (headless GUI for Firefox) ...\r\n"
-sudo apt-get -y install xvfb
+apt-get -y install xvfb
 
 
 # Finally, starting up Selenium server
