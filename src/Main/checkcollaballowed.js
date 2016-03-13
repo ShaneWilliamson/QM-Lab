c85e8b91b87d2277680f178fea7108a7b4764d9c
CoreyHickson <corey.r.hickson@gmail.com>
2016-03-13 00:17:55 -0600

    Added in JSDoc and began on contracts.
    Added in JSDoc and made some custom templates which will
    allow for preconditions, postconditions, invariants, and
    history.

--- src/Main/checkcollaballowed.js
+++ src/Main/checkcollaballowed.js
@@ -1,33 +1,40 @@
-/*
-	This function resets the global state logic to allow changes made to cells to
-	update their respective collaborative object once more.
-	
-	pre: 
-	post: the global state should once more allow updates to collab objects
-	*/
-	function allowCollabRecording() {
-		collaborativeChangeReceived = false;
-	}
-	
-	/*
-	This function changes the global state logic to not allow changes made to cells to
-	update their respective collaborative object any more.
-	
-	pre: 
-	post: the global state should not allow updates to collab objects
-	*/
-	function stopCollabRecording() {
-		collaborativeChangeReceived = true;
-	}
-	
-	/*
-	This function returns true if the global state is currently set to allow local objects
-	to update their collab counterparts.
-	
-	pre: 
-	post: 
-	return: if updates to collab objects are currently allowed
-	*/
-	function isCollabRecordingAllowed() {
-		return collaborativeChangeReceived;
-	}
+//////////////////////////
+// check_collab_allowed //
+//////////////////////////
+/**
+ * Manages if a collaborative object can be changed or not
+ * @class check_collab_allowed
+ * @invariant the object can either be changed or not
+ * @preconditions the object exists, the object is collaborative
+ */
+
+/**
+ * This function resets the global state logic to allow changes made to cells to
+ *   update their respective collaborative object once more.
+ * @preconditions the object exists, the object is collaborative
+ * @postconditions the object will allow changes
+ * @memberOf check_collab_allowed
+ */
+function allowCollabRecording() {
+	collaborativeChangeReceived = false;
+}
+
+/**
+ * This function changes the global state logic to not allow changes made to
+ *   cells to update their respective collaborative object any more.
+ * @preconditions the object exists, the object is collaborative
+ * @postconditions the object will not allow changes
+ * @memberOf check_collab_allowed
+ */
+function stopCollabRecording() {
+	collaborativeChangeReceived = true;
+}
+
+/**
+ * @preconditions the object exists, the object is collaborative
+ * @return {Boolean} if the object can be changed
+ * @memberOf check_collab_allowed
+ */
+function isCollabRecordingAllowed() {
+	return collaborativeChangeReceived;
+}
