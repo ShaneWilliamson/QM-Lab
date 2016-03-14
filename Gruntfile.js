99ece2e13669772194dd96989eaec7c83ba4270c
Jordan Wong <cooldued59@hotmail.com>
2016-03-14 15:15:42 -0600

    Simplified Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,110 +1,34 @@
-/*
- * grunt-contrib-qunit
- * http://gruntjs.com/
- *
- * Copyright (c) 2016 "Cowboy" Ben Alman, contributors
- * Licensed under the MIT license.
- */
-
-'use strict';
-
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'src/Main/*.js',
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+    pkg: grunt.file.readJSON('package.json'),
 
-    // Create a local web server for testing http:// URIs.
-    connect: {
-      root_server: {
-        options: {
-          port: 9000,
-          base: '.',
-        }
-      }
-    },
-
-    // Unit tests.
     qunit: {
-      all_tests: ['src/Unit_Tests/unitTest.html'],
-      individual_tests: {
-        files: [
-          {src: 'src/Unit_Tests/unitTest.html'}
-        ]
+      options: {
+      	'--ignore-ssl-errors':true,
+      	'--max-disk-cache-size':100000,
+      	'--load-images':false,
+      	'--local-to-remote-url-access':true,
+      	'--ssl-protocol':'any',
+        '--cookies-file':'cookies.txt'
       },
-      urls: {
-        options: {
-          urls: [
-            'http://localhost:9000/src/Unit_Tests/unitTest.html'
-          ]
-        }
-      },
-      urls_and_files: {
-        options: {
-          urls: '<%= qunit.urls.options.urls %>'
-        },
-        src: 'src/Unit_Tests/unitTest.html'
-      },
-      noglobals: {
-        options: {
-          noGlobals: true,
-          urls: [
-            'http://localhost:9000/src/Unit_Tests/unitTest.html'
-          ]
-        }
+      all: {
+      	options: {
+      		urls: [
+      			'http://test.local/src/Unit_Tests/unitTest.html'
+      		],
+      		page: {
+      			settings: {
+      				resourceTimeout:10000,
+      				//userAgent:'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
+      				loadImages:false,
+      				localToRemoteUrlAccessEnabled:true,
+      			}
+      		}
+      	}
       }
     }
 
   });
-
-  // Build a mapping of url success counters.
-  var successes = {};
-  var currentUrl;
-  grunt.event.on('qunit.spawn', function(url) {
-    currentUrl = url;
-    if (!successes[currentUrl]) { successes[currentUrl] = 0; }
-  });
-  grunt.event.on('qunit.done', function(failed, passed) {
-    if (failed === 0 && passed === 2) { successes[currentUrl]++; }
-  });
-
-  grunt.registerTask('really-test', 'Test to see if qunit task actually worked.', function() {
-    var assert = require('assert');
-    var difflet = require('difflet')({indent: 2, comment: true});
-    var actual = successes;
-    var expected = {
-      'test/src/Unit_Tests/unitTest.html': 3,
-      'http://localhost:9000/test/src/Unit_Tests/unitTest.html': 2,
-    };
-    try {
-      assert.deepEqual(actual, expected, 'Actual should match expected.');
-    } catch (err) {
-      grunt.log.subhead('Actual should match expected.');
-      console.log(difflet.compare(expected, actual));
-      throw new Error(err.message);
-    }
-  });
-
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-internal');
-
-  // Whenever the "test" task is run, run some basic tests.
-  grunt.registerTask('test', ['jshint', 'connect', 'qunit', 'really-test']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['test', 'build-contrib']);
-
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.registerTask('default', ['qunit']);
 };
