edd64c369a28b513b212beaafdc69664a8788f32
Jordan Wong <cooldued59@hotmail.com>
2016-03-14 14:59:35 -0600

    Test and see what happens to Travis.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,110 @@
+/*
+ * grunt-contrib-qunit
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2016 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
 module.exports = function(grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        qunit: {
-            files: ['test/index.html']
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'src/Main/*.js',
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Create a local web server for testing http:// URIs.
+    connect: {
+      root_server: {
+        options: {
+          port: 9000,
+          base: '.',
         }
-    });
+      }
+    },
 
-    // Load plugin
-    grunt.loadNpmTasks('grunt-contrib-qunit');
+    // Unit tests.
+    qunit: {
+      all_tests: ['src/Unit_Tests/unitTest.html'],
+      individual_tests: {
+        files: [
+          {src: 'src/Unit_Tests/unitTest.html'}
+        ]
+      },
+      urls: {
+        options: {
+          urls: [
+            'http://localhost:9000/src/Unit_Tests/unitTest.html'
+          ]
+        }
+      },
+      urls_and_files: {
+        options: {
+          urls: '<%= qunit.urls.options.urls %>'
+        },
+        src: 'src/Unit_Tests/unitTest.html'
+      },
+      noglobals: {
+        options: {
+          noGlobals: true,
+          urls: [
+            'http://localhost:9000/src/Unit_Tests/unitTest.html'
+          ]
+        }
+      }
+    }
 
-    // Task to run tests
-    grunt.registerTask('test', 'qunit');
+  });
+
+  // Build a mapping of url success counters.
+  var successes = {};
+  var currentUrl;
+  grunt.event.on('qunit.spawn', function(url) {
+    currentUrl = url;
+    if (!successes[currentUrl]) { successes[currentUrl] = 0; }
+  });
+  grunt.event.on('qunit.done', function(failed, passed) {
+    if (failed === 0 && passed === 2) { successes[currentUrl]++; }
+  });
+
+  grunt.registerTask('really-test', 'Test to see if qunit task actually worked.', function() {
+    var assert = require('assert');
+    var difflet = require('difflet')({indent: 2, comment: true});
+    var actual = successes;
+    var expected = {
+      'test/src/Unit_Tests/unitTest.html': 3,
+      'http://localhost:9000/test/src/Unit_Tests/unitTest.html': 2,
+    };
+    try {
+      assert.deepEqual(actual, expected, 'Actual should match expected.');
+    } catch (err) {
+      grunt.log.subhead('Actual should match expected.');
+      console.log(difflet.compare(expected, actual));
+      throw new Error(err.message);
+    }
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-internal');
+
+  // Whenever the "test" task is run, run some basic tests.
+  grunt.registerTask('test', ['jshint', 'connect', 'qunit', 'really-test']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['test', 'build-contrib']);
+
 };
---
+++ gruntSetup.sh
@@ -0,0 +1,3 @@
+sudo apt-get purge nodejs npm
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get -y install nodejs
