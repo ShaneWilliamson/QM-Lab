1ec43d3d2113359d7b7eaf88658fab68af17cb79
Michael <mjk121@mail.usask.ca>
2016-03-18 11:57:45 -0600

    Fix of create button.

--- src/Main/genUI.js
+++ src/Main/genUI.js
@@ -113,7 +113,7 @@ QM_LabUI.prototype.genToolbar = function() {
 	});   
 
 	$$("create").attachEvent("onItemClick", function(id, e){		
-        window.open(location.protocol + '//' + location.origin + location.pathname, '_blank');
+        window.open(location.origin + location.pathname, '_blank');
 	});
 
 	$$("save").attachEvent("onItemClick", function(id, e){		
