0c221904b23d3423fed4ce442e784b2c7a4034bb
Shane Williamson <williamson.shane.a@gmail.com>
2016-04-02 00:16:44 -0600

    Flows are now flows.
    There is still no valves on the flows, that's to come.
    As well there is a known bug with reloading the page causing the flows
    to turn into regular links.
    
    [ ticket: #46, #62 ]

--- src/Main/qmlabjointclasses.js
+++ src/Main/qmlabjointclasses.js
@@ -382,8 +382,9 @@
 
 	*/
 	function localFlow (pos, label, source, target, connector) {
-		var newFlow = new joint.shapes.QMLab.localLink();
+		var newFlow = new joint.shapes.QMLab.localFlow();
 		var localPos = $.extend(true, {}, pos);
+		console.log("We're in the flow place!");
 
 		//If a source was passed, set the link's source that
 		if (source) {
@@ -424,187 +425,6 @@
 
 		return newFlow;
 	}
-
-
-
-var FlowLinkView = joint.dia.LinkView.extend({
-
-    patternMarkup: [
-        '<pattern id="pattern-<%= id %>" patternUnits="userSpaceOnUse">',
-        '<image xlink:href=""/>',
-        '</pattern>'
-    ].join(''),
-
-    initialize: function() {
-
-        joint.dia.LinkView.prototype.initialize.apply(this, arguments);
-
-        _.bindAll(this, 'fillWithPattern');
-    },
-
-    render: function() {
-
-        joint.dia.LinkView.prototype.render.apply(this, arguments);
-
-        // make sure that pattern doesn't already exist
-        if (!this.pattern) {
-            
-            this.pattern = V(_.template(this.patternMarkup, { id: this.id }));
-            this.patternImage = this.pattern.findOne('image');
-
-            V(this.paper.svg).defs().append(this.pattern);
-        }
-
-        // tell the '.connection' path to use the pattern
-        var connection = V(this.el).findOne('.connection').attr({
-            stroke: 'url(#pattern-' + this.id + ')'
-        });
-
-        // cache the stroke width
-        this.strokeWidth = connection.attr('stroke-width') || 1;
-
-        return this;
-    },
-
-    remove: function() {
-
-        // make sure we stop an ongoing pattern update
-        joint.util.cancelFrame(this.frameId);
-
-        joint.dia.LinkView.prototype.remove.apply(this, arguments);
-
-        this.pattern.remove();
-    },
-    
-    update: function() {
-
-        joint.dia.LinkView.prototype.update.apply(this, arguments);
-
-        joint.util.cancelFrame(this.frameId);
-
-        this.frameId = joint.util.nextFrame(this.fillWithPattern);
-
-        return this;
-    },
-
-    fillWithPattern: function() {
-
-        var strokeWidth = this.strokeWidth;
-
-        // we get the bounding box of the linkView without the transformations
-        // and expand it to all 4 sides by the stroke width
-        // (making sure there is always enough room for drawing,
-        // even if the bounding box was tiny.
-        // Note that the bounding box doesn't include the stroke.)
-        var bbox = g.rect(V(this.el).bbox(true)).moveAndExpand({
-            x: - strokeWidth,
-            y: - strokeWidth,
-            width: 2 * strokeWidth,
-            height: 2 * strokeWidth
-        });
-
-        // create an array of all points the link goes through
-        // (route doesn't contain the connection points)
-        var points = [].concat(this.sourcePoint, this.route, this.targetPoint);
-
-        // transform all points to the links coordinate system
-        points = _.map(points, function(point) {
-            return g.point(point.x - bbox.x, point.y - bbox.y);
-        });
-
-        // create a canvas of the size same as the link bounding box
-        var canvas = document.createElement('canvas');
-        canvas.width = bbox.width;
-        canvas.height = bbox.height;
-
-        var ctx = canvas.getContext('2d');
-        ctx.lineWidth = strokeWidth;
-        ctx.lineJoin = "round";
-        ctx.lineCap = "round";
-
-        // iterate over the points and execute the drawing function
-        // for each segment
-        for (var i=0, pointsCount = points.length - 1; i < pointsCount; i++) {
-
-            ctx.save();
-            
-            var gradientPoints = this.gradientPoints(points[i], points[i+1], strokeWidth);
-            var gradient = ctx.createLinearGradient.apply(ctx, gradientPoints);
-
-            this.drawPattern.call(this, ctx, points[i], points[i+1], strokeWidth, gradient);
-            
-            ctx.restore();
-        }
-
-        // generate data URI from the canvas
-        var dataUri = canvas.toDataURL('image/png');
-
-        // update the pattern image and the dimensions
-        this.pattern.attr(bbox);
-        this.patternImage.attr({ width: bbox.width, height: bbox.height, 'xlink:href': dataUri });
-    },
-
-    // finds a gradient with perpendicular direction to a link segment
-    gradientPoints: function(from, to, width) {
-
-        var angle = g.toRad(from.theta(to) - 90);
-        var center = g.line(from, to).midpoint();
-        var start = g.point.fromPolar(width / 2, angle, center);
-        var end = g.point.fromPolar(width / 2, Math.PI + angle, center);
-
-        return [start.x, start.y, end.x, end.y];
-    },
-
-    // A drawing function executed for all links segments.
-    drawPattern: function(ctx, from, to, width, gradient) {
-
-        var innerWidth = width - 4;
-        var outerWidth = width;            
-        var buttFrom = g.point(from).move(to, -outerWidth / 2);
-        var buttTo = g.point(to).move(from, -outerWidth / 2);
-
-        ctx.beginPath();
-        ctx.lineWidth = outerWidth;
-        ctx.strokeStyle = 'rgba(0,0,0,0.6)';
-
-        ctx.moveTo(from.x, from.y);
-        ctx.lineTo(to.x, to.y);
-        ctx.stroke();
-        ctx.closePath();
-
-        gradient.addColorStop(0.000, 'rgba(86, 170, 255, 1)');
-        gradient.addColorStop(0.500, 'rgba(255, 255, 255, 1)');
-        gradient.addColorStop(1.000, 'rgba(86, 170, 255, 1)');
-        
-        ctx.beginPath();
-        ctx.lineWidth = innerWidth;
-        ctx.strokeStyle = gradient;
-        ctx.moveTo(from.x, from.y);
-        
-        ctx.lineTo(to.x, to.y);
-        ctx.stroke();
-        ctx.closePath();
-
-        ctx.lineCap = "square";
-
-        ctx.beginPath();
-        ctx.lineWidth = innerWidth;
-        ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-        ctx.moveTo(from.x, from.y);
-        ctx.lineTo(buttFrom.x, buttFrom.y);
-        ctx.stroke();
-        ctx.closePath();
-
-        ctx.beginPath();
-        ctx.lineWidth = innerWidth;
-        ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-        ctx.moveTo(to.x, to.y);
-        ctx.lineTo(buttTo.x, buttTo.y);
-        ctx.stroke();
-        ctx.closePath();
-    }
-});
-
 
 
 	/*
@@ -864,8 +684,9 @@ var FlowLinkView = joint.dia.LinkView.extend({
 	/*
 	This is the shape definition of the "localFlow" type. It extends joint.js' Link,
 	allowing us to easily customize it.	By default, sets the Link to behave exactly
-	link a default joint.js basic Link.
+	like a default joint.js basic Link.
 
+	'.connection2' is the white line drawn over the black line to give the visual effect of 2 lines.
 	*/
 	joint.shapes.QMLab.localFlow = joint.dia.Link.extend({
 		defaults: joint.util.deepSupplement({
@@ -873,7 +694,8 @@ var FlowLinkView = joint.dia.LinkView.extend({
 			type: 'QMLab.localLink',
 			/*Creates the arrow head at one end of the link*/
 			attrs: {
-				'.connection': { stroke: '#000000', fill: '#ffffff'},
+				'.connection': { stroke: '#000000', 'stroke-width': 6},
+				'.connection2': { stroke: '#FFFFFF', 'stroke-width': 2},
 				'.marker-target': {stroke: '#000000', fill: '#000000', d: 'M 10 0 L 0 5 L 10 10 z'}
 			}
 		}, joint.dia.Link.prototype.defaults),
